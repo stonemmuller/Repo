80ebc71e4066c907ef5fdec3ad9a620255d225d2
stonemmuller <mrstonem@mail.ru>
2018-09-04 23:42:05 +0500

    Add index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+	class Account {
+
+		private $con;
+		private $errorArray;
+
+		public function __construct($con) {
+			$this->con = $con;
+			$this->errorArray = array();
+		}
+
+		public function login($un, $pw) {
+
+			$pw = md5($pw);
+
+			$query = mysqli_query($this->con, "SELECT * FROM users WHERE username='$un' AND password='$pw'");
+
+			if(mysqli_num_rows($query) == 1) {
+				return true;
+			}
+			else {
+				array_push($this->errorArray, Constants::$loginFailed);
+				return false;
+			}
+
+		}
